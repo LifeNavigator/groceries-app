bdffee77deb3e6b1fdf7c6fadd24b93fe2de1f74
Kel <kfrancisco97@gmail.com>
2022-08-26 08:46:29 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './index.css'
+import SideBar from './SideBar'
+
+function App() {
+
+
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/MainContent.jsx
@@ -0,0 +1,8 @@
+
+const MainContent = () => {
+    return null
+}
+
+const GroceryMeny = () => {
+    
+}
